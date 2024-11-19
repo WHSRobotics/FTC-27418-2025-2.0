c1c4dc01b506ee37f277dd800bdd2987622ea4c2
Christopher Gholmieh <gholmieh.chris@gmail.com>
2024-11-19 07:47:50 -0800

    Initialized new repository
    - Created a new repository with a cleaned TeamCode folder.
    
    - Created boilerplate files for autonomous and teleop.

---
+++ TeamCode/src/main/java/org/whitneyrobotics/ftc/teamcode/OpMode/Autonomous/WHSAutonomous.java
@@ -0,0 +1,22 @@
+// Written by: Christopher Gholmieh & Bo-Yun Hsu
+// Package:
+package org.whitneyrobotics.ftc.teamcode.OpMode.Autonomous;
+
+// Imports:
+import org.whitneyrobotics.ftc.teamcode.Extensions.OpModeEx.OpModeEx;
+
+
+// Class:
+public class WHSAutonomous extends OpModeEx {
+    // Initialization:
+    @Override
+    public void initInternal() {
+
+    }
+
+    // Loop:
+    @Override
+    protected void loopInternal() {
+
+    }
+}
